d44edc62ff7e37151100f8c9965846563b6b073a
Avilekkk <84607950+AvilekkkDev@users.noreply.github.com>
2021-05-22 13:51:37 +0200

    Update Golang-Armv8.sh

--- Golang-Armv8.sh
+++ Golang-Armv8.sh
@@ -3,7 +3,7 @@
 cd ~
 wget https://golang.org/dl/go1.16.4.linux-arm64.tar.gz
 tar -xzf go1.16.4.linux-arm64.tar.gz
-cd go1.16.4.linux-arm64
+cd go
 sudo cp -R * /usr/local/
 clear
 echo Installed :D
